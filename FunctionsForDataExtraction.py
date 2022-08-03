9d2220f7389b92b7638668e9003b7b3e610cfce7
pvpetrus <pvpetrus@wp.pl>
2022-08-03 19:35:03 +0200

    Changing names

--- FunctionsForDataExtraction.py
+++ FunctionsForDataExtraction.py
@@ -2,10 +2,13 @@ import lxml as lxml
 from bs4 import BeautifulSoup
 import requests
 
+
 def print_results(variable):
     print("-----------------start---------------------")
     print(variable)
     print("-----------------end---------------------")
+
+
 def scrap_data_indexes(symbol):
     # połączenie ze stroną bankier i pobranie strony z danym symbolem
     html_text = requests.get("https://www.bankier.pl/inwestowanie/profile/quote.html?symbol={}".format(symbol)).text
@@ -14,7 +17,8 @@ def scrap_data_indexes(symbol):
     indexes = str(indexes).replace("</a></td>\n", "</a></td>\n")
     indexes = BeautifulSoup(indexes, 'lxml').text.replace(
         """PrzynaleÅ¼noÅÄ do indeksÃ³w\n\n\n\n\nIndeks\nUdziaÅ""",
-        "").replace("\n\n", "\n").replace("\n\n", "\n").replace("SpÃ³Åka nie przynaleÅ¼y do Å¼adnego indeksu","").replace("PrzynaleÅ¼noÅÄ do indeksÃ³w","")
+        "").replace("\n\n", "\n").replace("\n\n", "\n").replace("SpÃ³Åka nie przynaleÅ¼y do Å¼adnego indeksu",
+                                                                "").replace("PrzynaleÅ¼noÅÄ do indeksÃ³w", "")
     indexes = "\n".join([line for line in indexes.split('\n') if line.strip() != ''])
     print_results(indexes)
     return indexes
@@ -29,7 +33,7 @@ def scrap_data_pointers(symbol):
     pointers = BeautifulSoup(pointers, 'lxml').text.replace("""WskaÅºniki gieÅdowe\n\n\n\n\n\n\n""", "").replace(
         "\n\n", "\n").replace("zÅ", "PLN")
     pointers = "\n".join([line for line in pointers.split('\n') if line.strip() != ''])
-    #print_results(pointers)
+    # print_results(pointers)
     return pointers
 
 
@@ -51,12 +55,12 @@ def scrap_data_announcements(symbol):
 def scrap_symbols():
     html_text = requests.get("https://www.bankier.pl/gielda/notowania/akcje").text
     soup = BeautifulSoup(html_text, 'lxml')
-    symbols=soup.find_all('td',class_="colWalor textNowrap")
-    symbols_bufor=[]
+    symbols = soup.find_all('td', class_="colWalor textNowrap")
+    symbols_bufor = []
     for symbol in symbols:
-        symbol=symbol.text
+        symbol = symbol.text
         symbol = "\n".join([line for line in symbol.split('\n') if line.strip() != ''])
-        #print(symbol)
+        # print(symbol)
         symbols_bufor.append(symbol)
 
     return symbols_bufor
