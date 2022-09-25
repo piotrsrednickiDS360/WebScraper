ebd22f23e0a2b5a269584b2ec2e4fe0aa35728d8
pvpetrus <pvpetrus@wp.pl>
2022-09-25 23:56:53 +0200

    no scrapping

--- TradingSupport/settings.py
+++ TradingSupport/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'TradingSupport', 'static'),
 )
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
