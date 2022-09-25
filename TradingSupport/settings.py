21128894ca90a6a9138e2b7e598cfd8934542430
pvpetrus <pvpetrus@wp.pl>
2022-09-26 00:14:52 +0200

    no scrapping

--- TradingSupport/settings.py
+++ TradingSupport/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'TradingSupport', 'static'),
+    os.path.join(BASE_DIR, 'TradingSupportApp', 'static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
